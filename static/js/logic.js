57f868413a14dde34196704670d5ed7d1eb3d0d4
BrianSilfer <briansilfer67@gmail.com>
2023-07-23 12:21:45 -0400

    update

--- static/js/logic.js
+++ static/js/logic.js
@@ -20,13 +20,38 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     let features = data.features
     //create an empty list for coordinates
     coordinates_list = [];
+    latitude_list = [];
+    longitude_list = [];
     depth_list = [];
     for (let i = 0; i < features.length; i++){
         let feature = features[i].geometry.coordinates;
-        coordinates_list.push(feature.slice(0,2))
-        depth_list =[];
-        let marker = L.marker(coordinates_list[i],{
-            draggable: true
+        let properties = features[i].properties.detail;
+        coordinates_list.push(feature.slice(0,2));
+        depth_list.push(parseInt(feature.slice(2,3)).toFixed(0));
+        let fillColor;
+
+        if(depth_list[i]>= -10 && depth_list[i] < 10){
+            fillColor = "green";
+        } else if (depth_list[i]>= 10 && depth_list[i] < 30){
+            fillColor = "red";
+        } else if (depth_list[i]>= 30 && depth_list[i] < 50){
+            fillColor = "blue";
+        } else if (depth_list[i]>= 50 && depth_list[i] < 70){
+            fillColor = "orange";
+        } else if (depth_list[i]>= 70 && depth_list[i] < 90){
+            fillColor = "yellow";
+        } else if (depth_list[i] > 90){
+            fillColor = "white";
+        }
+
+
+        
+        let marker = L.circle(coordinates_list[i].reverse(),{
+            radius: depth_list[i]*1100,
+            color: fillColor,
+            fillColor: fillColor
+            
+            
 
         }).addTo(myMap);
         // Binding a popup to our marker to check coordinates
@@ -34,6 +59,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     marker.bindPopup("Hello");
     };
     console.log(coordinates_list)
-    
+    console.log(depth_list)
+    //console.log(properties)
     
 })
