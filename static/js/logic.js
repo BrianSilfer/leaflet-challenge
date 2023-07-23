eeeac948ea8115f074841449e1ea33fb03ca708b
BrianSilfer <briansilfer67@gmail.com>
2023-07-23 13:29:53 -0400

    update

--- static/js/logic.js
+++ static/js/logic.js
@@ -18,17 +18,19 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     console.log(data)
     //define features to loop through data
     let features = data.features
-    //create an empty list for coordinates
+    //create an empty list for coordinates and depths
     coordinates_list = [];
     latitude_list = [];
     longitude_list = [];
     depth_list = [];
+    //write a for loop to loop through every feature and pull coordinates, depth to create markers
     for (let i = 0; i < features.length; i++){
         let feature = features[i].geometry.coordinates;
         coordinates_list.push(feature.slice(0,2));
         depth_list.push(parseInt(feature.slice(2,3)).toFixed(0));
+        //define fillColor
         let fillColor;
-
+        //write if else statements to match the color to the magnitude category based on mag size
         if(depth_list[i]>= -10 && depth_list[i] < 10){
             fillColor = "#54F253";
         } else if (depth_list[i]>= 10 && depth_list[i] < 30){
@@ -44,7 +46,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         }
 
 
-        
+        //create circle markers based on data pulled from each earthquake
         let marker = L.circle(coordinates_list[i].reverse(),{
             radius: depth_list[i]*1100,
             color: fillColor,
@@ -55,7 +57,8 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
             
 
         }).addTo(myMap);
-        // Binding a popup to our marker to check coordinates
+
+        // Binding a popup to our marker using a f-statment like feature of js
         let marker_message = `Magnitude: ${features[i].properties.mag} <br> 
         Location: ${features[i].properties.place} <br>
         Depth: ${depth_list[i]}`
@@ -64,7 +67,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         
     };
     
-
+    //initial console logs to check to see if data was being collected correctly
     console.log(coordinates_list)
     console.log(depth_list)
    
